6d016cef8691ad1de251f15c95583e286b685341
ItsAstro3 <105923392+ItsAstro3@users.noreply.github.com>
2022-05-26 20:42:12 +1000

    ss
    dfsf

--- htdocs/aboutme.php
+++ htdocs/aboutme.php
@@ -15,13 +15,12 @@
 <body>
 	<nav>
 		<ul class="navbar">
-			<li id="home"><a href="index.html">HOME</a></li>
-			<li id="gallery"><a href="gallery.html">GALLERY</a></li>
-			<li id="artists"><a href="aboutme.html">ARTISTS</a></li>
-			<li id="submit"><a href="submit.html">SUBMIT YOUR ART</a></li>
-			<li id="contactus"><a href="contactus.html">CONTACT US</a></li>
-			<li id="WIPpage"><a href="submit.html">SUBMIT YOUR ART</a></li>
-			<li id="database"><a href="database.html">CONTACT US</a></li>
+			<li id="home"><a href="index.php">HOME</a></li>
+			<li id="gallery"><a href="gallery.php">GALLERY</a></li>
+			<li id="artists"><a href="aboutme.php">ARTISTS</a></li>
+			<li id="submit"><a href="submit.php">SUBMIT YOUR ART</a></li>
+			<li id="contactus"><a href="contactus.php">CONTACT US</a></li>
+			<li id="WIPpage"><a href="extrapage.php">6TH PAGE</a></li>
 		</ul>
 	</nav>
 	<br>
